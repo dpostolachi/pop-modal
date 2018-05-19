c89413ab68808bbd7d1377c686bca4fbfdc037dd
Anakin <anakin@admins-iMac.local>
2018-05-19 20:02:57 +0300

    Using flow

--- dist/index.js
+++ dist/index.js
@@ -20,36 +20,39 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var defaultProps = {
+    onClose: null,
+    onOpen: null,
+    openMs: 1000,
+    closeMs: 500,
+    showBtn: true,
+    btnClassName: 'fa fa-close',
+    outsideClickClose: false,
+    children: [],
+    defaultOpen: false
+};
+
 // It's an decorator
 exports.default = function (Comp) {
 
     return function (_PureComponent) {
         _inherits(Modal, _PureComponent);
 
-        function Modal(props) {
+        function Modal() {
+            var _ref;
+
+            var _temp, _this, _ret;
+
             _classCallCheck(this, Modal);
 
-            var _this = _possibleConstructorReturn(this, (Modal.__proto__ || Object.getPrototypeOf(Modal)).call(this, props));
+            for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+                args[_key] = arguments[_key];
+            }
 
-            _this.state = {
+            return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Modal.__proto__ || Object.getPrototypeOf(Modal)).call.apply(_ref, [this].concat(args))), _this), _this.state = {
                 stage: 'closed',
                 ready: false
-
-                // Default values
-            };_this.onClose = null;
-            _this.onOpen = null;
-            _this.openMs = 1000;
-            _this.closeMs = 500;
-            _this.showBtn = true;
-            _this.btnClassName = 'fa fa-close';
-            _this.outsideClickClose = false;
-
-            _this.openPopup = _this.openPopup.bind(_this);
-            _this.closePopup = _this.closePopup.bind(_this);
-            _this.clickOutside = _this.clickOutside.bind(_this);
-            _this._modal = _this._modal.bind(_this);
-
-            return _this;
+            }, _this.openMs = 0, _this.closeMs = 0, _this.onClose = null, _this.onOpen = null, _temp), _possibleConstructorReturn(_this, _ret);
         }
 
         _createClass(Modal, [{
@@ -57,7 +60,7 @@ exports.default = function (Comp) {
             value: function openPopup() {
                 var _this2 = this;
 
-                var initialStage = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
+                var initialStage = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
 
 
                 if (initialStage) return this.setState({
@@ -129,36 +132,27 @@ exports.default = function (Comp) {
                 var ready = this.state.ready;
 
 
-                var stage = !ready && typeof props.defaultOpen !== 'undefined' && props.defaultOpen ? 'open' : this.state.stage;
+                var newProps = _extends({}, defaultProps, props);
 
-                if (typeof props.onClose === 'function') this.onClose = props.onClose;
+                var stage = !ready && newProps.defaultOpen ? this.setState({ stage: 'open' }) || 'open' : this.state.stage;
 
-                if (typeof props.onOpen === 'function') this.onOpen = props.onOpen;
-
-                if (typeof props.openMs === "number") this.openMs = props.openMs;
-
-                if (typeof props.closeMs === 'number') this.closeMs = props.closeMs;
-
-                if (typeof props.showBtn === 'boolean') this.showBtn = props.showBtn;
-
-                if (typeof props.btnClassName === 'string') this.btnClassName = props.btnClassName;
-
-                if (typeof props.outsideClickClose === 'boolean') this.outsideClickClose = props.outsideClickClose;
+                this.onClose = newProps.onClose || null;
+                this.onOpen = newProps.onOpen || null;
 
                 if (stage === 'closed') return null;
 
                 return _react2.default.createElement(
                     'div',
-                    { className: 'popModal ' + stage, key: '1', onClick: this.outsideClickClose && stage === 'open' ? this.clickOutside : null },
-                    this.showBtn && stage === 'open' ? _react2.default.createElement(
+                    { className: 'popModal ' + stage, key: '1', onClick: newProps.outsideClickClose && stage === 'open' ? this.clickOutside.bind(this) : null },
+                    newProps.showBtn && stage === 'open' ? _react2.default.createElement(
                         'button',
-                        { onClick: this.closePopup, className: 'popModalClose', type: 'button' },
-                        _react2.default.createElement('span', { className: this.btnClassName })
+                        { onClick: this.closePopup.bind(this), className: 'popModalClose', type: 'button' },
+                        _react2.default.createElement('span', { className: newProps.btnClassName })
                     ) : null,
                     _react2.default.createElement(
                         'div',
                         { className: 'popModalContainer' },
-                        props.children
+                        newProps.children
                     )
                 );
             }
@@ -166,7 +160,12 @@ exports.default = function (Comp) {
             key: 'render',
             value: function render() {
 
-                return _react2.default.createElement(Comp, _extends({}, this.props, { Modal: this._modal, modalStage: this.state.stage, openPopup: this.openPopup, closePopup: this.closePopup }));
+                return _react2.default.createElement(Comp, _extends({}, this.props, {
+                    Modal: this._modal.bind(this),
+                    modalStage: this.state.stage,
+                    openPopup: this.openPopup.bind(this),
+                    closePopup: this.closePopup.bind(this)
+                }));
             }
         }]);
 
--- index.js
+++ index.js
@@ -1,37 +1,52 @@
+/* @flow */
+
 import React, { PureComponent } from 'react'
 
+const defaultProps = {
+    onClose: null,
+    onOpen: null,
+    openMs: 1000,
+    closeMs: 500,
+    showBtn: true,
+    btnClassName: 'fa fa-close',
+    outsideClickClose: false,
+    children: [],
+    defaultOpen: false
+}
+
+type Props = {
+    onClose?: Function,
+    onOpen?: Function,
+    openMs?: number,
+    closeMs?: number,
+    showBtn?: boolean,
+    btnClassName?: string,
+    outsideClickClose?: boolean,
+    defaultOpen?: boolean,
+    children?: any,
+}
+
+type State = {
+    stage: string,
+    ready: boolean,
+}
+
 // It's an decorator
-export default ( Comp ) => {
+export default ( Comp: any ) => {
 
-    return class Modal extends PureComponent {
+    return class Modal extends PureComponent<any, State> {
 
-        constructor ( props ) {
-
-            super( props )
-
-            this.state = {
-                stage: 'closed',
-                ready: false,
-            }
-
-            // Default values
-            this.onClose = null
-            this.onOpen = null
-            this.openMs = 1000
-            this.closeMs = 500
-            this.showBtn = true
-            this.btnClassName = 'fa fa-close'
-            this.outsideClickClose = false
-
-
-            this.openPopup = this.openPopup.bind(this)
-            this.closePopup = this.closePopup.bind(this)
-            this.clickOutside = this.clickOutside.bind(this)
-            this._modal = this._modal.bind(this)
-
+        state: State = {
+            stage: 'closed',
+            ready: false
         }
 
-        openPopup ( initialStage = null ) {
+        openMs: number = 0
+        closeMs: number = 0
+        onClose: null | Function = null
+        onOpen: null | Function = null
+
+        openPopup ( initialStage?: string = '' ) {
 
                 if ( initialStage )
                     return this.setState( {
@@ -98,56 +113,41 @@ export default ( Comp ) => {
 
         }
 
-        clickOutside (e) {
+        clickOutside ( e: Object ) {
 
             if ( e.target.classList.contains( 'popModal' ) )
                 return this.closePopup()
 
         }
 
-        _modal ( props ) {
+        _modal ( props: Props ) {
 
             const { ready } = this.state
 
-            const stage = ( !ready && typeof props.defaultOpen !== 'undefined' && props.defaultOpen ) ? 'open' : this.state.stage
+            const newProps = { ...defaultProps, ...props }
 
-            if ( typeof props.onClose === 'function' )
-                this.onClose = props.onClose
+            const stage = ( !ready && newProps.defaultOpen ) ? this.setState({  stage: 'open' }) || 'open' : this.state.stage
 
-            if ( typeof props.onOpen === 'function' )
-                this.onOpen = props.onOpen
 
-            if ( typeof props.openMs === "number" )
-                this.openMs = props.openMs
-
-            if ( typeof props.closeMs === 'number' )
-                this.closeMs = props.closeMs
-
-            if ( typeof props.showBtn === 'boolean' )
-                this.showBtn = props.showBtn
-
-            if ( typeof props.btnClassName === 'string' )
-                this.btnClassName = props.btnClassName
-
-            if ( typeof props.outsideClickClose === 'boolean' )
-                this.outsideClickClose = props.outsideClickClose
+            this.onClose = newProps.onClose || null
+            this.onOpen = newProps.onOpen || null
 
             if ( stage === 'closed' )
                 return null
 
             return (
-                <div className={ `popModal ${stage}` } key='1' onClick={ ( this.outsideClickClose && stage === 'open' ) ? this.clickOutside : null }>
+                <div className={ `popModal ${stage}` } key='1' onClick={ ( newProps.outsideClickClose && stage === 'open' ) ? this.clickOutside.bind( this ) : null }>
                     {
-                        ( this.showBtn && stage === 'open' ) ? (
+                        ( newProps.showBtn && stage === 'open' ) ? (
 
-                            <button onClick={ this.closePopup } className='popModalClose' type='button'>
-                                <span className={ this.btnClassName } />
+                            <button onClick={ this.closePopup.bind( this ) } className='popModalClose' type='button'>
+                                <span className={ newProps.btnClassName } />
                             </button>
 
                         ) : null
                     }
                     <div className='popModalContainer'>
-                        { props.children }
+                        { newProps.children }
                     </div>
                 </div>
             )
@@ -155,7 +155,13 @@ export default ( Comp ) => {
 
         render () {
 
-            return <Comp { ...this.props } Modal={ this._modal } modalStage={ this.state.stage } openPopup={ this.openPopup } closePopup={ this.closePopup } />
+            return <Comp
+                { ...this.props }
+                Modal={ this._modal.bind( this ) }
+                modalStage={ this.state.stage }
+                openPopup={ this.openPopup.bind( this ) }
+                closePopup={ this.closePopup.bind( this ) }
+            />
 
         }
 
